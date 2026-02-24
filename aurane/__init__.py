45e78ce12bd96ce3304008acb849d29de87f1b93
Naitik Gupta <60667035+desenyon@users.noreply.github.com>
2026-02-24 14:09:07 -0800

    chore: bump version to 1.0.2 for PyPI release

--- aurane/__init__.py
+++ aurane/__init__.py
@@ -4,7 +4,7 @@ Aurane - ML-Oriented DSL that Transpiles to Python
 A domain-specific language for writing ML code that compiles to idiomatic Python.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.2"
 
 from .compiler import compile_file, compile_source, CompilationError
 from .parser import parse_aurane, ParseError
